59bfa5983a176b4dd4e823e16564b25936a02b89
Erenus Dermanci <erenus@mail.com>
2022-08-12 17:41:04 +0200

    Adjust indices to match 0-based array indexing

--- src/Program.cs
+++ src/Program.cs
@@ -9,34 +9,35 @@ namespace Sandbox
     {
         public static void Main(string[] args)
         {
-            MikeAndShortcuts(3, new int[] { -1, 2, 2, 3 });
-            MikeAndShortcuts(5, new int[] { -1, 1, 2, 3, 4, 5 });
-            MikeAndShortcuts(7, new int[] { -1, 4, 4, 4, 4, 7, 7, 7 });
+            MikeAndShortcuts(3, new int[] { 2, 2, 3 });
+            MikeAndShortcuts(5, new int[] { 1, 2, 3, 4, 5 });
+            MikeAndShortcuts(7, new int[] { 4, 4, 4, 4, 7, 7, 7 });
         }
 
         private static int[] MikeAndShortcuts(int n, int[] shortcuts)
         {
-            var minCosts = new int[n + 1];
-            minCosts[0] = -1; // dummy
+            var minCosts = new int[n];
 
             var shortcutsHashMap = new Dictionary<int, int>(); // key: starting position, value: destination
             var minShortcutCostForDestination =
                 new Dictionary<int, int[]>(); // key: destination, value: starting position [0] and cost [1]
 
             // fill up our shortcut data
-            for (var i = 1; i <= n; ++i)
+            for (var i = 0; i < n; ++i)
             {
-                if (!minShortcutCostForDestination.ContainsKey(shortcuts[i]))
-                    minShortcutCostForDestination[shortcuts[i]] = new[] { i, i };
-                else if (minShortcutCostForDestination[shortcuts[i]][1] > i)
-                    minShortcutCostForDestination[shortcuts[i]] = new[] { i, i };
+                var destinationIdx = shortcuts[i] - 1;
+                if (!minShortcutCostForDestination.ContainsKey(destinationIdx))
+                    minShortcutCostForDestination[destinationIdx] = new[] { i, i };
+                else if (minShortcutCostForDestination[destinationIdx][1] > i)
+                    minShortcutCostForDestination[destinationIdx] = new[] { i, i };
 
-                shortcutsHashMap[i] = shortcuts[i];
+                shortcutsHashMap[i] = destinationIdx;
             }
 
-            for (var i = 1; i <= n; ++i)
+            minCosts[0] = 0;
+            for (var i = 1; i < n; ++i)
             {
-                var cost = Math.Min(i - 1, minCosts[i - 1] + 1); // base cost
+                var cost = Math.Min(i, minCosts[i - 1] + 1); // base cost
 
                 if (minShortcutCostForDestination.ContainsKey(i))
                 {
