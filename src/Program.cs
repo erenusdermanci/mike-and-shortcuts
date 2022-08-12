ee3d85eebe255f4b38401954a73fd6b9f929ff5a
Erenus Dermanci <erenus@mail.com>
2022-08-12 23:51:03 +0200

    Use queue, accepted submission

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -26,111 +25,42 @@ namespace Sandbox
 
                 argCount--;
             }
-            Console.WriteLine(string.Join(' ', MikeAndShortcuts2(n, shortcuts).Select(val => val.ToString()).ToArray()));
+
+            Console.WriteLine(string.Join(' ', MikeAndShortcuts(n, shortcuts).Select(val => val.ToString()).ToArray()));
         }
 
         private static int[] MikeAndShortcuts(int n, int[] shortcuts)
         {
             var minCosts = new int[n];
+            var queue = new Queue<int>();
 
-            var shortcutsHashMap = new Dictionary<int, int>(); // key: starting position, value: destination
-            var minShortcutCostForDestination =
-                new Dictionary<int, int[]>(); // key: destination, value: starting position [0] and cost [1]
+            queue.Enqueue(0);
 
-            // fill up our shortcut data
-            for (var i = 0; i < n; ++i)
+            while (queue.Count > 0)
             {
-                var destinationIdx = shortcuts[i] - 1;
-                if (!minShortcutCostForDestination.ContainsKey(destinationIdx))
-                    minShortcutCostForDestination[destinationIdx] = new[] { i, i };
-                else if (minShortcutCostForDestination[destinationIdx][1] > i)
-                    minShortcutCostForDestination[destinationIdx] = new[] { i, i };
+                var x = queue.Dequeue();
 
-                shortcutsHashMap[i] = destinationIdx;
-            }
-
-            minCosts[0] = 0;
-            for (var i = 1; i < n; ++i)
-            {
-                var cost = Math.Min(i, minCosts[i - 1] + 1); // base cost
-
-                if (minShortcutCostForDestination.ContainsKey(i))
+                if (x + 1 < n && minCosts[x + 1] == 0)
                 {
-                    var costWithShortcut = minShortcutCostForDestination[i][1] + 1;
-                    if (costWithShortcut < cost)
-                    {
-                        cost = costWithShortcut;
-                    }
+                    minCosts[x + 1] = minCosts[x] + 1;
+                    queue.Enqueue(x + 1);
                 }
 
-                if (shortcutsHashMap.ContainsKey(i))
+                if (x - 1 > 0 && minCosts[x - 1] == 0)
                 {
-                    if (minShortcutCostForDestination[shortcutsHashMap[i]][1] > cost)
-                    {
-                        minShortcutCostForDestination[shortcutsHashMap[i]][0] = i;
-                        minShortcutCostForDestination[shortcutsHashMap[i]][1] = cost;
-                    }
+                    minCosts[x - 1] = minCosts[x] + 1;
+                    queue.Enqueue(x - 1);
                 }
-                
-                minCosts[i] = cost;
-            }
 
-            for (var i = n - 1 - 1; i > 0; --i)
-            {
-                if (minCosts[i - 1] > minCosts[i] + 1)
-                    minCosts[i - 1] = minCosts[i] + 1;
-            }
-
-            return minCosts;
-        }
-
-        private static int[] MikeAndShortcuts2(int n, int[] shortcuts)
-        {
-            var minCosts = new int[n];
-            var visited = new bool[n];
-
-            for (var i = 0; i < n; ++i)
-            {
-                minCosts[i] = int.MaxValue;
-                visited[i] = false;
-            }
-
-            minCosts[0] = 0;
-
-            for (var i = 0; i < n - 1; ++i)
-            {
-                var unvisited = UnvisitedMinimumIndex(minCosts, visited, n);
-                visited[unvisited] = true;
-
-                for (var j = 0; j < n; ++j)
+                var shortcut = shortcuts[x] - 1;
+                if (shortcut > x && minCosts[shortcut] == 0)
                 {
-                    var minCostU2J = Math.Min(Math.Abs((unvisited +  1) - (j + 1)), shortcuts[unvisited] == j + 1 ? 1 : int.MaxValue);
-                    if (!visited[j] && minCosts[unvisited] != int.MaxValue &&
-                        minCosts[unvisited] + minCostU2J < minCosts[j])
-                    {
-                        minCosts[j] = minCosts[unvisited] + minCostU2J;
-                    }
+                    minCosts[shortcut] = minCosts[x] + 1;
+                    queue.Enqueue(shortcut);
                 }
             }
 
             return minCosts;
-        }
-        
-        private static int UnvisitedMinimumIndex(int[] minCosts, bool[] visited, int n)
-        {
-            var min = int.MaxValue;
-            var minIndex = -1;
-
-            for (var i = 0; i < n; ++i)
-            {
-                if (visited[i] == false && minCosts[i] <= min)
-                {
-                    min = minCosts[i];
-                    minIndex = i;
-                }
-            }
-
-            return minIndex;
         }
     }
 }
